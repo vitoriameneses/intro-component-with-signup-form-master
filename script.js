711f43cebd722eb128f3564745261ee30f011b9c
Vitória Teixeira <60278157+vitoriameneses@users.noreply.github.com>
2023-06-01 12:12:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    //get the form values by the ids
+    
+    const firstName = form['firstname'].value;
+    const lastName = form['lastname'].value;
+    const email = form['email'].value;
+    const password = form['password'].value;
+    
+    if(firstName === '') {
+        addErrorTo('firstname', 'First name is required' );
+    } else {
+		removeErrorFrom('firstname');
+	}
+    if(lastName === '') {
+        addErrorTo('lastname', 'Last name is required' );
+    } else {
+		removeErrorFrom('lastname');
+	}
+    if(isValid(email)) {
+        addErrorTo('email', 'Email is not valid' );
+    } else if (email == '') {
+        addErrorTo('email', 'Email is required' );
+    } else {
+		removeErrorFrom('email');
+	}
+    if(password === '') {
+        addErrorTo('password', 'Password is required' );
+    } else {
+		removeErrorFrom('password');
+	}
+});
+function addErrorTo(field, message) {
+    const formControl = form[field].parentNode;
+	formControl.classList.add('error');
+
+	const small = formControl.querySelector('small');
+	small.innerText = message;
+}
+function removeErrorFrom(field) {
+	const formControl = form[field].parentNode;
+	formControl.classList.remove('error');
+}
+function isValid(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+function addErrorTo(field, message) {
+	const formControl = form[field].parentNode;
+	formControl.classList.add('error');
+
+	const small = formControl.querySelector('small');
+	small.innerText = message;
+}
+
+function removeErrorFrom(field) {
+	const formControl = form[field].parentNode;
+	formControl.classList.remove('error');
+}
+function isValid(email) {
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
